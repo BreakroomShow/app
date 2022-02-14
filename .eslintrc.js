ed8f303d2b2e00a267d9ae01ca5dd4dd72a62c7f
maxcheremisin <maxcheremisin2008@gmail.com>
2022-02-14 20:26:08 +0300

    fix Multiple instances of Three.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,8 +39,10 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'max-classes-per-file': 'off',
         'no-use-before-define': 'off',
+        'class-methods-use-this': 'off',
 
         'import/extensions': 'off',
+        'import/no-default-export': 'error',
         'import/prefer-default-export': 'off',
         'import/no-extraneous-dependencies': ['error', { optionalDependencies: true }],
 
@@ -83,6 +85,7 @@ module.exports = {
             rules: {
                 'import/no-anonymous-default-export': 'off',
                 'import/no-extraneous-dependencies': 'off',
+                'import/no-default-export': 'off',
             },
         },
         {
--- config-overrides.js
+++ config-overrides.js
@@ -5,5 +5,19 @@ module.exports = function override(webpackConfig) {
         type: 'javascript/auto',
     })
 
+    webpackConfig.externals = [
+        (context, request, callback) => {
+            if (request === 'three' && context.includes('framer-motion-3d')) {
+                return callback(null, {
+                    commonjs: 'three',
+                    commonjs2: 'three',
+                    amd: 'three',
+                    root: 'THREE',
+                })
+            }
+            callback()
+        },
+    ]
+
     return webpackConfig
 }
