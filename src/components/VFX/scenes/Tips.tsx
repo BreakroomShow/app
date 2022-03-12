1c23ca5a1c7e9c334ae5d2736daefe3cdea5f94c
Vlad <vladmdgolam@ya.ru>
2022-03-12 18:29:18 +0400

    new animation for crypto facts

--- src/components/VFX/scenes/Tips.tsx
+++ src/components/VFX/scenes/Tips.tsx
@@ -1,10 +1,15 @@
-import { Box } from '@react-three/drei'
 import { useState } from 'react'
 
 import { CryptoFactEvent } from '../../../types'
 import { AnimatePresence } from '../components/AnimatePresence'
 import { Diamonds } from '../components/Diamonds'
 import { FlatText } from '../components/FlatText'
+
+const variants = {
+    enter: { scale: 0.001 },
+    visible: { scale: 1 },
+    exit: { scale: 0.001 },
+}
 
 export const Tips = ({ event: nextEvent }: { event: CryptoFactEvent }) => {
     const [event, setEvent] = useState(nextEvent)
@@ -14,6 +19,7 @@ export const Tips = ({ event: nextEvent }: { event: CryptoFactEvent }) => {
                 id={event.text}
                 onExit={() => setEvent(nextEvent)}
                 isVisible={nextEvent.text === event.text}
+                variants={variants}
             >
                 <FlatText text={event.text} widthCoeff={0.33} />
             </AnimatePresence>
